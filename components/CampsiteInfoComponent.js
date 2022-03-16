fffd684cafe08eceaba26c6eee69502ffa71b898
Allie Pistolessi <a.pistol17@gmail.com>
2022-03-16 15:54:48 -0400

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {
     Text, View, ScrollView, FlatList,
     Modal, Button, StyleSheet,
-    Alert, PanResponder
+    Alert, PanResponder, Share
 } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
@@ -64,6 +64,17 @@ function RenderCampsite(props) {
         }
     })
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title,
+            message: `${title}: ${message} ${url}`,
+            url
+        },
+            {
+                dialogTitle: 'Share ' + title
+            })
+    }
+
     if (campsite) {
         return (
             <Animatable.View
@@ -90,12 +101,20 @@ function RenderCampsite(props) {
                                 console.log('Already set as a favorite') : props.markFavorite()}
                         />
                         <Icon
-                            name='pencil'
+                            name={'pencil'}
                             type='font-awesome'
                             color='#5637DD'
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
                         />
                     </View>
                 </Card>
